579115cd76b97067dd291cd1bc4b24df857048b4
Nemanja Djordjevic <djordjeviccnemanjaa@gmail.com>
2025-06-25 01:57:16 +0200

    zavrseno skoro sve

--- assets/js/scrollcue.js
+++ assets/js/scrollcue.js
@@ -490,3 +490,20 @@
     }
 
 })();
+
+var swiper = new Swiper(".mySwiper", {
+    loop: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+    },
+  });
+
